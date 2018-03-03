85126e5570a741267ce30f79e2589adbd979da46
Vladislav Leonov <vlad_leonov@bk.ru>
2018-03-03 17:38:17 +0300

    Changing main.c for testing create_systematic_view method

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 int main() {
 
-	matrix message = create_empty_matrix(1, 20);
+	/*matrix message = create_empty_matrix(1, 20);
 	int i;
 	for (i = 0; i < 20; i++) {
 		message.body[0][i] = 'a' + i;
@@ -15,7 +15,25 @@ int main() {
 	
 	for (i = 0; i < 20; i++) {
 		printf("%c ", message.body[0][i]);
-	}
+	}*/
+	
+	matrix M = create_zero_matrix(3, 5);
+	M.body[0][0] = 1;
+	M.body[0][1] = 1;
+	M.body[0][3] = 1;
+	M.body[1][1] = 1;
+	M.body[1][2] = 1;
+	M.body[1][4] = 1;
+	M.body[2][0] = 1;
+	M.body[2][2] = 1;
+	M.body[2][3] = 1;
+	
+	printf("G = \n");
+	print_matrix(M);
+	printf("\n");
+	
+	create_systematic_view(M);
+	
 	system("pause");
 	return 0;
 }
