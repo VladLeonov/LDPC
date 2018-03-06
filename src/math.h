17d8a14cc46b82f6c2e8d1ddfd7547a55d8fc13e
Gotus <stalnoe5555@mail.ru>
2018-03-06 19:42:32 +0300

    Some mistakes were fixed.

--- src/math.h
+++ src/math.h
@@ -2,7 +2,7 @@
 #define MATH
 
 int* gauss_elimination(matrix G);
-ldpc create_systematic_view(matrix G);
-int sum_rows(matrix G, int row_index)
+ldpc create_systematic_view(matrix G_old);
+int sum_rows(matrix G, int row_index);
 
 #endif
