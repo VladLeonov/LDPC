a0b924404024fe24e46259c64052e6aabd48e91b
Vladislav Leonov <vlad_leonov@bk.ru>
2018-03-06 19:11:58 +0300

    Update math.h

--- src/math.h
+++ src/math.h
@@ -1,4 +1,8 @@
-#include "matrix.h"
-#include "ldpc.h"
+#ifndef MATH
+#define MATH
 
+int* gauss_elimination(matrix G);
 ldpc create_systematic_view(matrix G);
+int sum_rows(matrix G, int row_index)
+
+#endif
