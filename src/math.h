bd52e9a743c871acf07b237ca61ca76a0c5aafe6
Vladislav Leonov <vlad_leonov@bk.ru>
2018-03-09 22:13:05 +0300

    Add fill_with_permutation to .h file

--- src/math.h
+++ src/math.h
@@ -4,5 +4,6 @@
 int* gauss_elimination(matrix G);
 ldpc create_systematic_view(matrix G_old, char is_H_entered);
 int sum_rows(matrix G, int row_index);
+void fill_with_permutation(int *x, int n);
 
 #endif
