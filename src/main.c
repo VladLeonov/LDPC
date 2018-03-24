3239d6335fbcda1ca125c304d9ca70f12ac9a06b
Gotus <stalnoe5555@mail.ru>
2018-03-24 19:51:35 +0300

    Nothing changed.

--- src/main.c
+++ src/main.c
@@ -11,17 +11,17 @@ int main() {
     /*int J = 2, K = 5, M = 4;
     matrix H = create_H_rand(J, K, M);
 	ldpc ldpc_object = create_systematic_view(H, 1);
-	
+
 	int message_size = M * (K - J);
 	int message[1][message_size];
 	int i;
 	for (i = 0; i < message_size; i++) {
 		message[0][i] = i % 2;
 	}
-	
+
 	matrix encoded_message = encode(ldpc_object, array_to_matrix(1, message_size, message));
 	matrix syndrome = count_syndrome(ldpc_object, encoded_message);
-	
+
 	printf("J = %d, K = %d, M = %d\n\n", J, K, M);
 	printf("Random H =\n");
 	print_matrix(H);
@@ -32,9 +32,9 @@ int main() {
 	print_ldpc(ldpc_object);
 	printf("Syndrome =\n");
 	print_matrix(syndrome);
-	
+
     system("pause");
-    
+
     free_matrix(H);
     free_matrix(encoded_message);
     free_matrix(syndrome);*/
