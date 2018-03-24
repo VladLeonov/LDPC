791360f864234b4548d5dde7f481cd6c2b221627
Vladislav Leonov <vlad_leonov@bk.ru>
2018-03-24 19:41:29 +0300

    Rewriting encode method for using H instead of G.

--- src/ldpc.c
+++ src/ldpc.c
@@ -3,7 +3,30 @@
 #include "math.h"
 
 matrix encode(ldpc ldpc_object, matrix message) {
-    return multiply_matrices(message, ldpc_object.G);
+    //return multiply_matrices(message, ldpc_object.G);
+    int n = ldpc_object.n;
+	int k = ldpc_object.k;
+	int r = n - k;
+    matrix data = copy_matrix_part(message, 1, k);
+    matrix codeword = create_zero_matrix(1, n);
+    
+    int* check_set = ldpc_object.columns_mdata.check_set;
+    int* information_set = ldpc_object.columns_mdata.information_set;
+
+    int i, j;
+    for (i = 0; i < k; i++) {
+    	codeword.body[0][information_set[i]] = data.body[0][i];
+	}
+	
+	for (i = 0; i < r; i++) {
+		for (j = 0; j < k; j++) {
+			codeword.body[0][check_set[i]] ^= data.body[0][j] * ldpc_object.H.body[i][information_set[j]];
+		} 
+	}
+	
+	free_matrix(data);
+	
+	return codeword;
 }
 
 matrix count_syndrome(ldpc ldpc_object, matrix codedword) {
