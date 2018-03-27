2c6789afc2abc9b5b3891ba74f94d9529c09b932
Vladislav Leonov <vlad_leonov@bk.ru>
2018-03-27 20:41:55 +0300

    Rewriting main.c for new methods

--- src/main.c
+++ src/main.c
@@ -2,41 +2,38 @@
 #include "matrix.h"
 #include "math.h"
 
+#define TRUE !0
+#define FALSE 0
+
 int main() {
 
     int J = 2, K = 5, M = 4;
     ldpc ldpc_object = create_ldpc(J, K, M);
     print_ldpc(ldpc_object);
-    system("pause");
-    /*int J = 2, K = 5, M = 4;
-    matrix H = create_H_rand(J, K, M);
-	ldpc ldpc_object = create_systematic_view(H, 1);
-
-	int message_size = M * (K - J);
+    
+    int message_size = M * (K - J) + J - 1;
 	int message[1][message_size];
 	int i;
 	for (i = 0; i < message_size; i++) {
 		message[0][i] = i % 2;
 	}
-
-	matrix encoded_message = encode(ldpc_object, array_to_matrix(1, message_size, message));
-	matrix syndrome = count_syndrome(ldpc_object, encoded_message);
-
-	printf("J = %d, K = %d, M = %d\n\n", J, K, M);
-	printf("Random H =\n");
-	print_matrix(H);
-	printf("\n");
-	printf("Modified H =\n");
-	print_matrix(copy_matrix_part(ldpc_object.H, M * J - J + 1, ldpc_object.H.columns));
-	printf("\n");
-	print_ldpc(ldpc_object);
-	printf("Syndrome =\n");
-	print_matrix(syndrome);
-
+	
+	matrix encoded_message = encode(ldpc_object, array_to_matrix(1, message_size, message), FALSE);
+	matrix syndrome = count_syndrome(ldpc_object, encoded_message, FALSE);
+	
+	printf("encoded_message =\n");
+    print_matrix(encoded_message);
+    printf("\n");
+    
+    printf("syndrome =\n");
+    print_matrix(syndrome);
+    printf("\n");
+    
     system("pause");
-
-    free_matrix(H);
+    
+    free_ldpc(ldpc_object);
     free_matrix(encoded_message);
-    free_matrix(syndrome);*/
+    free_matrix(syndrome);
+
     return 0;
 }
