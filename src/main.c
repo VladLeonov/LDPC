51f53c5cc6b8545b1d691d174fc45dc59480a604
Vladislav Leonov <vlad_leonov@bk.ru>
2018-04-10 17:34:01 +0300

    Rewriting main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "ldpc.h"
 #include "matrix.h"
 #include "math.h"
+#include "ldpc_tester.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -10,59 +11,19 @@
 
 int main() {
 
-    int J = 2, K = 5, M = 4;
+    int J = 9, K = 18, M = 8;
     ldpc ldpc_object = create_ldpc(RU_code, J, K, M);
+    
+    SNR_interval SNR = {0.0, 1.0, 0.1};
+    FILE *file = fopen("decoding_simulation.txt", "w");
+    decoding_simulation(ldpc_object, SNR, file);
+    fclose(file);
 
-    int message_size = M * (K - J) + J - 1;
-	int message[1][message_size];
-	int i;
-	for (i = 0; i < message_size; i++) {
-		message[0][i] = i % 2;
-	}
-
-	matrix encoded_message = encode(ldpc_object, array_to_matrix(1, message_size, message), TRUE);
-
-	printf("encoded_message =\n");
-    print_matrix(encoded_message);
-    printf("\n");
-
-    float *recieved_message = (float*) malloc(encoded_message.columns * sizeof(float));
-
-	for (i = 0; i < encoded_message.columns; i++) {
-        recieved_message[i] = 2 - 4 * encoded_message.body[0][i];
-	}
-
-	recieved_message[8] *= -0.5;//-2
-
-    matrix *hard_solution = (matrix*)malloc(sizeof(matrix));
-
-    printf("Iterations = %d\n", decode_belief_propogandation(ldpc_object, recieved_message, hard_solution, FALSE));
-
-    printf("hard_solution =\n");
-    print_matrix(*hard_solution);
-
-    for (i = 0; i < encoded_message.columns; i++) {
-		printf(encoded_message.body[0][i] == hard_solution->body[0][i] ? ". " : "- ");
-	}
-	printf("\n\n");
-
-	int *hard = (int*) malloc(encoded_message.columns * sizeof(int));
-	printf("Iterations = %d\n", flooding(recieved_message, hard, ldpc_object));
-	printf("hard =\n");
-	for (i = 0; i < encoded_message.columns; i++) {
-		printf("%d ", hard[i]);
-	}
-	printf("\n");
-    for (i = 0; i < encoded_message.columns; i++) {
-		printf(encoded_message.body[0][i] == hard[i] ? ". " : "- ");
-	}
-
-	//printf("%s\n", compare_matrices(create_unit_matrix(2), create_zero_matrix(2, 2)));
-    print_matrix(create_random_message(24));
+    //int message_size = M * (K - J) + J - 1;
+    
     system("pause");
 
     free_ldpc(ldpc_object);
-    free_matrix(encoded_message);
 
     return 0;
 }
