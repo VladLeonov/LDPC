84d0404a03102f4f7116ce5de32b219d70d0765f
Vladislav Leonov <vlad_leonov@bk.ru>
2018-05-04 17:22:17 +0300

    Adding remove_edge_from_graph method

--- src/tanner_graph.c
+++ src/tanner_graph.c
@@ -41,3 +41,33 @@ graph get_tanner_graph_from_ldpc(ldpc ldpc_object) {
 	
 	return tanner_graph;
 }
+
+
+void remove_edge_from_graph(graph graph_object, int vertex_index) {
+	if (graph_object.degree_of_vertices[vertex_index] == 0) {
+		return;
+	}
+	
+	int other_vertex_index = graph_object.adjacency_list[vertex_index][0];
+	
+	int i;
+	for (i = 0; i < graph_object.degree_of_vertices[vertex_index] - 1; i++) {
+		graph_object.adjacency_list[vertex_index][i] = graph_object.adjacency_list[vertex_index][i + 1];
+	}
+	
+	graph_object.degree_of_vertices[vertex_index]--;
+	graph_object.adjacency_list[vertex_index] = 
+		(int*) realloc(graph_object.adjacency_list[vertex_index], 
+		graph_object.degree_of_vertices[vertex_index] * sizeof(int));
+		
+	for (i = 0; graph_object.adjacency_list[other_vertex_index][i] != vertex_index; i++);
+	
+	for (; i < graph_object.degree_of_vertices[other_vertex_index] - 1; i++) {
+		graph_object.adjacency_list[other_vertex_index][i] = graph_object.adjacency_list[other_vertex_index][i + 1];
+	}
+	
+	graph_object.degree_of_vertices[other_vertex_index]--;
+	graph_object.adjacency_list[other_vertex_index] = 
+		(int*) realloc(graph_object.adjacency_list[other_vertex_index], 
+		graph_object.degree_of_vertices[other_vertex_index] * sizeof(int));
+}
--- src/tanner_graph.h
+++ src/tanner_graph.h
@@ -10,5 +10,6 @@ typedef struct {
 } graph;
 
 graph get_tanner_graph_from_ldpc(ldpc ldpc_object);
+void remove_edge_from_graph(graph graph_object, int vertex_index);
 
 #endif // TANNER_GRAPH_CORRECT_VERSION_H_INCLUDED
