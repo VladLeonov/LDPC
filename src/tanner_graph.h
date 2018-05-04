e64d08fb232dffbddc5226e2918af5a32fcbf23d
Vladislav Leonov <vlad_leonov@bk.ru>
2018-05-04 15:39:28 +0300

    Adding tanner_graph with structure graph

---
+++ src/tanner_graph.h
@@ -0,0 +1,11 @@
+
+#ifndef TANNER_GRAPH_CORRECT_VERSION_H_INCLUDED
+#define TANNER_GRAPH_CORRECT_VERSION_H_INCLUDED
+
+typedef struct {
+	int number_of_vertices;
+    int **adjacency_list;
+    int *degree_of_vertices;
+} graph;
+
+#endif // TANNER_GRAPH_CORRECT_VERSION_H_INCLUDED
